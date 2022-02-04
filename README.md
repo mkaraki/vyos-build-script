c832afc8edfbd56549e9bd42851855d0c30136a2
mkaraki <26180919+mkaraki@users.noreply.github.com>
2022-02-05 00:23:20 +0900

    Update scripts

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # vyos-build-script
+
+See [.github/workflows/build.yml](.github/workflows/build.yml) to check build steps.
--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# From: https://docs.vyos.io/en/latest/contributing/build-vyos.html#build-iso
-
-./configure --architecture amd64
-make iso
