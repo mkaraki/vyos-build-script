6d4c79510d2f3848346d820b18381d282ee59a39
mkaraki <26180919+mkaraki@users.noreply.github.com>
2022-02-04 22:38:50 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,5 @@
 
 # From: https://docs.vyos.io/en/latest/contributing/build-vyos.html#build-iso
 
-git clone -b equuleus --single-branch https://github.com/vyos/vyos-build /vyos
-
-cd /vyos
-
 ./configure --architecture amd64
 make iso
